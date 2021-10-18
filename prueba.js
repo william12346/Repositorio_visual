04176eb52ea012cabfabb13f63c11f849be58d37
William Avila <wdavila@unillanos.edu.co>
2021-10-18 18:32:36 -0500

    Mi primer commit con giMi primer commit con git

---
+++ prueba.js
@@ -0,0 +1,32 @@
+//document.write("<h1>Hola mundo </h1>");
+// LET: Se agrega siempre antes de crear una variable
+let nombre ="jose";
+
+const PI = 3.141516
+
+let num1 =1234;
+let num3 =567;
+let resultado = num1 > num3;
+
+/* document.write: mostrar en la pagina
+
+document.write(resultado);
+*/
+/* console.log: mostrar en consola
+
+console.log(resultado);
+*/
+// Condicionales
+/*
+if (num1 == num3){
+  console.log("son iguales");
+}
+else{
+  console.log("Son diferentes");
+}
+*/
+let list=["p","i","o"];
+let dicc = {
+"usuario" :"jose",
+"pass" : 123
+}
